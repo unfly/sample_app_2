2248e1bd143563f8bb51fd818144b2b2f9ad0b08
unfly <unflys@gmail.com>
2014-03-25 11:12:13 +0800

    'Q' 不是内部或外部命令，也不是可运行的程序 或批处理文件。

---
+++ README.md
@@ -0,0 +1,2 @@
+#sample application
+unfly的第二个练习,办公室，winxp
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '8086e5316d8c53d4551192b2a0563693db3d307d4681d7d4144b6092de7b1b4a8d2ce93d151ce5b9720a3d976ca20ee7ebdab2f9a5f258b3729e0ceb655e81c7'
